1e529fe92260c4c376808699271341e5ec9a8102
dorottyat <dorottya.torok.97@gmail.com>
2022-07-12 12:05:12 +0200

    configured h2 database in application.properties, made Recipe an entity

--- src/main/java/com/dorottyat/recipe/Recipe.java
+++ src/main/java/com/dorottyat/recipe/Recipe.java
@@ -1,8 +1,17 @@
 package com.dorottyat.recipe;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.ArrayList;
 
+@Entity
 public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name;
     private int serving;
     private int time;
